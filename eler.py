76d1f7863d8eb3fe94f701d91a2cffb736fdd793
Pedro <114117383+Anxiemon@users.noreply.github.com>
2023-12-13 14:24:57 -0300

    Add files via upload

--- eler.py
+++ eler.py
@@ -1,9 +1,10 @@
-mode = input("mode? (write/read) ")
+mode = input("mode? (write/read) \n")
 if mode == "write":
     arqwrite = open(input("file name: "), "w")
-    arqwrite.write(input("text/input: "))
+    arqwrite.write(input("text/input: \n"))
 elif mode == "read":
     arqread = open(input("file name: "), "r")
-    print("text: ", arqread.read())
+    print("text: \n", arqread.read())
+    input("\n Press Enter to exit")
 else:
     exit()
