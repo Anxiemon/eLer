8d78cce70aa9b83fa3e2e3b2a8d19c1ebc74154a
Pedro <114117383+Anxiemon@users.noreply.github.com>
2023-12-13 19:58:24 -0300

    v0.0.2

--- eler.py
+++ eler.py
@@ -1,10 +1,13 @@
-mode = input("mode? (write/read) \n")
-if mode == "write":
-    arqwrite = open(input("file name: "), "w")
-    arqwrite.write(input("text/input: \n"))
-elif mode == "read":
-    arqread = open(input("file name: "), "r")
-    print("text: \n", arqread.read())
-    input("\n Press Enter to exit")
-else:
-    exit()
+active = True
+while active:
+    mode = input("mode? (write/read/exit) \n")
+    if mode == "write":
+        arqwrite = open(input("file name: "), "w")
+        arqwrite.write(input("text/input: \n"))
+    elif mode == "read":
+        arqread = open(input("file name: "), "r")
+        print("text: \n", arqread.read())
+    elif mode == "exit":
+        active = False
+    else:
+        print("I think you made a typo.")
