d2fe7b322febd297e5ebb3ac2f9b2b4f5b6f35f5
Pedro <114117383+Anxiemon@users.noreply.github.com>
2023-12-14 20:34:25 -0300

    v0.0.3

--- eler.py
+++ eler.py
@@ -1,12 +1,15 @@
 active = True
 while active:
     mode = input("mode? (write/read/exit) \n")
+    # asks what mode the user wants to use
     if mode == "write":
         arqwrite = open(input("file name: "), "w")
         arqwrite.write(input("text/input: \n"))
+        arqwrite.close()
     elif mode == "read":
         arqread = open(input("file name: "), "r")
         print("text: \n", arqread.read())
+        arqread.close()
     elif mode == "exit":
         active = False
     else:
